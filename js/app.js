e67924b8fad7788cd8ec5c639cd407b42983368b
maribeltorresg <maribel.torres.guevara@gmail.com>
2017-11-20 04:46:27 -0500

    version 0.0.3

--- js/app.js
+++ js/app.js
@@ -20,11 +20,29 @@ window.addEventListener('load', function() {
     for (var i = 0; i < data.length; i++) {
       var list = createElement('div', 'list');
       var listTitle = createElement('div', 'list-title', data[i].title);
+
       var cards = createElement('div', 'cards');
-      var cardAdd = createElement('div', 'card-add', 'Add a card...');
-      cardAdd.addEventListener('click', function() {
-        console.log('adding card');
+      // version 3
+      var cardAdd = createElement('div', 'card-add inactive');
+      var cardAddPlaceholder = createElement('span', 'card-add-placeholder', 'Add a card...');
+      var cardAddTextarea = createElement('textarea', 'card-add-textarea');
+      var cardAddSave = createElement('div', 'button button-done', 'Save');
+      var cardAddCancel = createElement('div', 'button', null, 'span class="fa fa-close"></span>');
+
+      cardAddPlaceholder.addEventListener('click', function() {
+        cardAdd.classList.remove('inactive');
       });
+      
+      cardAddCancel.addEventListener('click', function() {
+        cardAdd.classList.add('inactive');
+      });
+
+      cardAdd.appendChild(cardAddPlaceholder);
+      cardAdd.appendChild(cardAddTextarea);
+      cardAdd.appendChild(cardAddSave);
+      cardAdd.appendChild(cardAddCancel);
+      // fin version 3
+
       list.appendChild(listTitle);
       list.appendChild(cards);
       list.appendChild(cardAdd);
